2acbe14c27b1f2a36f54957e2dab123a8877a98d
ouch-ouch <78248901+ouch-ouch@users.noreply.github.com>
2022-06-08 21:35:43 +0530

    RightBiotic Commit

---
+++ app/src/main/java/com/example/rightbiotic/HelperDataStore.java
@@ -0,0 +1,2 @@
+package com.example.rightbiotic;public class HelperDataStore {
+}
---
+++ app/src/main/java/com/example/rightbiotic/login1.java
@@ -0,0 +1,14 @@
+package com.example.rightbiotic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login1);
+    }
+}
